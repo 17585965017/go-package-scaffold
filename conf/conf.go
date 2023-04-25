d6435c8f8d2cdf47cc52752fdc256634807c4e33
刘健 <liujian@ydjdev.com>
2023-04-25 17:09:55 +0800

    fix: util.BuildLogger("debug")

--- conf/conf.go
+++ conf/conf.go
@@ -2,12 +2,11 @@ package conf
 
 import (
 	"go_package_scaffold/util"
-	"os"
 )
 
 // Init 初始化配置项
 func Init() {
 
 	// 设置日志级别
-	util.BuildLogger(os.Getenv("LOG_LEVEL"))
+	util.BuildLogger("debug")
 }
