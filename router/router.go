a35b5119e0e3303e5ff480895cc72590fb071957
刘健 <liujian@ydjdev.com>
2023-04-25 17:50:53 +0800

    feat: configure cors sessions cookie middleware and join the project

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"go_package_scaffold/api/v1"
+	"go_package_scaffold/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,9 @@ import (
 // NewRouter 路由配置
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(middleware.Session("setOnProduction"))
+	r.Use(middleware.Cors())
 
 	// 路由配置
 	v1 := r.Group("/api/v1")
